9aaa84acecd343ea850a3afa09ef1dbe02a1135f
Eden(peter) <peterchinokoo@gmail.com>
2023-11-13 22:46:40 +0200

    Implement Searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import gamify from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="5px">
       <Image src={gamify} boxSize="50px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
