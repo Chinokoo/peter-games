c4d95c50aca22c12fd9180af9be512547cbc701b
Eden(peter) <peterchinokoo@gmail.com>
2023-11-14 17:58:38 +0200

    Customize The Theme to get darker grays

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,21 @@
+import bullsEye from "../assets/bulls-eye.webp";
+import thumsup from "../assets/thumbs-up.webp";
+import meh from "../assets/meh.webp";
+import { Image, ImageProps } from "@chakra-ui/react";
+
+interface Props {
+  rating: number;
+}
+
+const Emoji = ({ rating }: Props) => {
+  if (rating < 3) return null;
+
+  const emojiMap: { [key: number]: ImageProps } = {
+    3: { src: meh, alt: "meh" },
+    4: { src: thumsup, alt: "recommended" },
+    5: { src: bullsEye, alt: "exceptional" },
+  };
+  return <Image {...emojiMap[rating]} boxSize="25px" />;
+};
+
+export default Emoji;
